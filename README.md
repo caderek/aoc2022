084589ab4ea89203e436ef798221eb756ff94452
Maciej Caderek <maciej.caderek@gmail.com>
2022-12-02 22:33:38 +0100

    Solutons day 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.475ms
-Time part 2: 0.626ms
-Both parts: 1.101691ms
+Time part 1: 0.58ms
+Time part 2: 0.696ms
+Both parts: 1.2759209999999999ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.855ms
+Time part 2: 2.553ms
+Both parts: 5.4080960000000005ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.102ms
+Total stars: 4/50
+Total time: 6.684ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,35 +1,61 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput: string) => rawInput
+type Move = "A" | "B" | "C"
+type Response = "X" | "Y" | "Z"
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((line) => line.split(" ")) as [Move, Response][]
+
+const names = { X: "A", Y: "B", Z: "C" }
+const values = { A: 1, B: 2, C: 3 }
+const wins = { A: "C", B: "A", C: "B" }
+const loses = { C: "A", A: "B", B: "C" }
+const outcomes = { X: 0, Y: 3, Z: 6 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  return input
+    .map(([a, b]) => {
+      const v = names[b] as Move
+      return (a === v ? 3 : wins[v] === a ? 6 : 0) + values[v]
+    })
+    .reduce((a, b) => a + b)
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  return input
+    .map(([a, b]) => {
+      const item = (b === "X" ? wins[a] : b === "Y" ? a : loses[a]) as Move
+      return outcomes[b] + values[item]
+    })
+    .reduce((a, b) => a + b)
 }
+
+const testInput = `
+  A Y
+  B X
+  C Z
+`
 
 run({
   part1: {
     tests: [
       {
-        input: ``,
-        expected: "",
+        input: testInput,
+        expected: 15,
       },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: testInput,
+        expected: 12,
+      },
     ],
     solution: part2,
   },
