dc9216f1d04cec68205c8fa568ebf89410049dec
Maciej Caderek <maciej.caderek@gmail.com>
2022-12-08 16:46:28 +0100

    Solutons day 8

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -111,16 +111,16 @@ Both parts: 7.232046ms
 
 ```
 Day 07
-Time part 1: 3.614ms
-Time part 2: 3.45ms
-Both parts: 7.063732ms
+Time part 1: 3.474ms
+Time part 2: 2.535ms
+Both parts: 6.009555000000001ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 6.331ms
+Time part 2: 15.32ms
+Both parts: 21.651102ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 32.944ms
+Total stars: 16/50
+Total time: 53.541ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day08/index.ts
@@ -0,0 +1,119 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((line) => line.split("").map(Number))
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  const visible = new Set<string>()
+
+  for (let y = 0; y < input.length; y++) {
+    let max = -1
+    for (let x = 0; x < input[0].length; x++) {
+      if (input[y][x] > max) {
+        visible.add(`${y}:${x}`)
+        max = input[y][x]
+      }
+    }
+
+    max = -1
+    for (let x = input[0].length - 1; x >= 0; x--) {
+      if (input[y][x] > max) {
+        visible.add(`${y}:${x}`)
+        max = input[y][x]
+      }
+    }
+  }
+
+  for (let x = 0; x < input[0].length; x++) {
+    let max = -1
+    for (let y = 0; y < input.length; y++) {
+      if (input[y][x] > max) {
+        visible.add(`${y}:${x}`)
+        max = input[y][x]
+      }
+    }
+
+    max = -1
+    for (let y = input[0].length - 1; y >= 0; y--) {
+      if (input[y][x] > max) {
+        visible.add(`${y}:${x}`)
+        max = input[y][x]
+      }
+    }
+  }
+
+  return visible.size
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  const dirs = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ]
+
+  const scenicScores: number[] = []
+
+  for (let y = 1; y < input.length - 1; y++) {
+    for (let x = 1; x < input[0].length - 1; x++) {
+      const treeHeight = input[y][x]
+      let scenicScore = 1
+
+      for (const [dY, dX] of dirs) {
+        let v = 0
+        let i = 0
+
+        while (true) {
+          i++
+          const next = input[y + dY * i]?.[x + dX * i]
+
+          if (next !== undefined) {
+            v++
+          }
+
+          if (next >= treeHeight || next === undefined) {
+            break
+          }
+        }
+        scenicScore *= v
+      }
+
+      scenicScores.push(scenicScore)
+    }
+  }
+
+  return Math.max(...scenicScores)
+}
+
+const testInput = `
+  30373
+  25512
+  65332
+  33549
+  35390
+`
+
+run({
+  part1: {
+    tests: [
+      {
+        input: testInput,
+        expected: 21,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: testInput,
+        expected: 8,
+      },
+    ],
+    solution: part2,
+  },
+})
