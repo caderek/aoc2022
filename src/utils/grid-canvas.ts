5f710694a19e02cddd1cd0c84d58dc6b2110db2d
Maciej Caderek <maciej.caderek@gmail.com>
2022-12-11 01:26:57 +0100

    Grid visualisation WIP

--- src/utils/grid-canvas.ts
+++ src/utils/grid-canvas.ts
@@ -1,10 +1,10 @@
 type Shape = "square" | "circle"
+type Color = string | null
 
 type Config = {
-  colors: {
-    background: string
-    slot: string
-    tile: string
+  fill: {
+    slot: Color
+    tile: Color
   }
   sizes: {
     tile: number
@@ -14,39 +14,51 @@ type Config = {
     tile: Shape
     slot: Shape
   }
+  stroke: {
+    tile: Color
+    slot: Color
+  }
+  background: Color
   showSlots: boolean
 }
 
-type TileData = {
-  color?: string
-  size?: number
+type TileStyle = {
+  fill?: Color
+  stroke?: Color
+  scale?: number
   shape?: Shape
 }
 
-type Grid = (string | number | boolean | TileData)[][]
-type Points = ({ x: number; y: number } & TileData)[]
+type GridCell = string | number | boolean
+type Point = { x: number; y: number }
+
+type Format<T> = (item: T) => TileStyle
 
 const defaultConfig: Config = {
-  colors: {
-    background: "#000",
-    slot: "#111",
+  fill: {
+    slot: "#140a1c",
     tile: "#eee",
   },
   sizes: {
-    tile: 20,
-    gap: 10,
+    tile: 8,
+    gap: 2,
   },
   shapes: {
     tile: "square",
     slot: "square",
   },
-  showSlots: false,
+  stroke: {
+    tile: null,
+    slot: null,
+  },
+  background: "#0e0614",
+  showSlots: true,
 }
 
 class GridCanvas {
   #canvas: HTMLCanvasElement
   #ctx: CanvasRenderingContext2D
-  #config: Config
+  #config: Config = defaultConfig
 
   #resize = () => {
     this.#canvas.width = this.#canvas.clientWidth
@@ -62,74 +74,141 @@ class GridCanvas {
 
     this.#canvas = canvas as HTMLCanvasElement
     this.#ctx = this.#canvas.getContext("2d") as CanvasRenderingContext2D
-    this.#config = { ...defaultConfig, ...config }
 
+    this.#updateConfig(config)
+    this.#resize()
     this.#registerResizeHandler()
+
+    this.#canvas.style.background = this.#config.background ?? "transparent"
+  }
+
+  #updateConfig(config: Partial<Config> = {}) {
+    if (config.showSlots) {
+      this.#config.showSlots = config.showSlots
+    }
+    if (config.fill) {
+      this.#config.fill = { ...this.#config.fill, ...config.fill }
+    }
+    if (config.shapes) {
+      this.#config.shapes = { ...this.#config.shapes, ...config.shapes }
+    }
+    if (config.sizes) {
+      this.#config.sizes = { ...this.#config.sizes, ...config.sizes }
+    }
   }
 
   #registerResizeHandler() {
     window.addEventListener("resize", this.#resize)
   }
 
-  #rect(x: number, y: number, color: string, size: number) {
-    const offset = (config.tileSize * (1 - size)) / 2
-    this.#ctx.fillStyle = color
-    this.#ctx.fillRect(
-      x * config.tileSize + config.gap * x + offset,
-      y * config.tileSize + config.gap * y + offset,
-      config.tileSize * size,
-      config.tileSize * size,
-    )
+  #square(x: number, y: number, fill: Color, stroke: Color, scale: number = 1) {
+    const tile = this.#config.sizes.tile
+    const gap = this.#config.sizes.gap
+    const offset = (tile * (1 - scale)) / 2
+
+    if (fill) {
+      this.#ctx.fillStyle = fill
+      this.#ctx.fillRect(
+        x * (tile + gap) + offset,
+        y * (tile + gap) + offset,
+        tile * scale,
+        tile * scale,
+      )
+    }
+
+    if (stroke) {
+      this.#ctx.strokeStyle = stroke
+      this.#ctx.lineWidth = 1
+      this.#ctx.strokeRect(
+        x * (tile + gap) + offset,
+        y * (tile + gap) + offset,
+        tile * scale,
+        tile * scale,
+      )
+    }
   }
 
-  draw(data: Grid | Points) {}
+  #circle(x: number, y: number, fill: Color, stroke: Color, scale: number = 1) {
+    const tile = this.#config.sizes.tile
+    const gap = this.#config.sizes.gap
+    const size = this.#config.sizes.tile * scale
+    const offset = (this.#config.sizes.tile * (1 - scale)) / 2 + size / 2
+
+    this.#ctx.beginPath()
+    this.#ctx.arc(
+      x * (tile + gap) + offset,
+      y * (tile + gap) + offset,
+      size / 2,
+      0,
+      Math.PI * 2,
+    )
+
+    if (fill) {
+      this.#ctx.fillStyle = fill
+      this.#ctx.fill()
+    }
+
+    if (stroke) {
+      this.#ctx.strokeStyle = stroke
+      this.#ctx.lineWidth = 1
+      this.#ctx.stroke()
+    }
+  }
+
+  #getShapeFn(shape: Shape) {
+    if (shape === "circle") {
+      return this.#circle.bind(this)
+    }
+
+    return this.#square.bind(this)
+  }
+
+  drawGrid<T extends GridCell>(data: T[][], format?: Format<T>) {
+    console.log(this.#config)
+
+    const height = data.length
+    const width = data[0].length
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        let gridCell = data[y][x]
+
+        if (typeof gridCell === "string") {
+          // @ts-ignore
+          gridCell = gridCell.trim()
+        }
+
+        if (this.#config.showSlots) {
+          this.#getShapeFn(this.#config.shapes.slot)(
+            x,
+            y,
+            this.#config.fill.slot,
+            this.#config.stroke.slot,
+          )
+        }
+
+        const style: Required<TileStyle> = {
+          fill: this.#config.fill.tile,
+          stroke: this.#config.stroke.tile,
+          scale: 1,
+          shape: this.#config.shapes.tile,
+          ...(format ? format(gridCell) : {}),
+        }
+
+        if (gridCell) {
+          this.#getShapeFn(style.shape)(
+            x,
+            y,
+            style.fill,
+            style.stroke,
+            style.scale,
+          )
+        }
+      }
+    }
+  }
+
+  drawPoints<T extends Point>(data: T, format?: Format<T>) {}
 }
 
-// const grid = [
-//   [1, 0, 0, 0, 0, 1, 0],
-//   [0, 0, 0, 0, 1, 1, 0],
-//   [1, 0, 0, 1, 0, 1, 1],
-//   [1, 1, 0, 0, 1, 1, 0],
-//   [0, 0, 1, 0, 0, 1, 1],
-//   [1, 0, 0, 1, 0, 1, 0],
-//   [0, 1, 0, 1, 0, 1, 1],
-// ]
-
-// const config = {
-//   tileSize: 20,
-//   tileSlotColor: "#160b1f",
-//   gap: 10,
-// }
-
-// const rect = (x: number, y: number, color: string, size: number) => {}
-
-// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
-// const draw = async () => {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height)
-//   for (let y = 0; y < grid.length; y++) {
-//     for (let x = 0; x < grid.length; x++) {
-//       if (config.tileSlotColor) {
-//         rect(x, y, "#160b1f")
-//       }
-
-//       // if (grid[y][x]) {
-//       //   rect(x, y, "#f55", 0.8;
-//       // }
-
-//       if (Math.random() > 0.5) {
-//         rect(x, y, "#f55", Math.random())
-//       }
-//     }
-//   }
-
-//   await delay(100)
-//   requestAnimationFrame(draw)
-// }
-
-// draw()
-
-// window.addEventListener("resize", () => {
-//   canvas.width = canvas.clientWidth
-//   canvas.height = canvas.clientHeight
-// })
+export default GridCanvas
